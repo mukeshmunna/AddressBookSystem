e949b8696801062bc35154e72112f6fd596de5ed
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-17 12:50:13 +0530

    [Mukesh] Add : Contact

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book problem statement");
+            AddressBooks address = new AddressBooks();
+            address.CreateContact();
+
+        }
+    }
+}
